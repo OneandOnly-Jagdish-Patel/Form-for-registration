f7b5d35a87397604876d2fe3970229ccb3526b42
OneandOnly-Jagdish-Patel <pate5749@sheridancollege.ca>
2024-10-18 03:51:19 -0400

    Ready to publish made

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, url_for, request,redirect
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    Phone = request.form['phone']
+    mandal = request.form['occupation']
+    october31 = request.form['ans']
+    november1 = request.form['1november']
+    november2 = request.form['2november']
+
+    mandal_dict = {
+    'NorthYork' : 'https://sheetdb.io/api/v1/gxmmko96zjybv',
+    'Etobicoke' : 'https://sheetdb.io/api/v1/sbr6q3rnah0vk',
+    'Brampton' : 'https://sheetdb.io/api/v1/7b1cl5do1qg81',
+    'master_url' : 'https://sheetdb.io/api/v1/ix3s63q2syihq',
+    }
+    user_data = {
+            "name" : name,
+            "email" : email,
+            "Phone" : Phone,
+            "mandal" : mandal,
+            "october31" : october31,
+            "november1" : november1,
+            "november2" : november2
+        }
+    row = {
+        "sheet1" : {
+            "Name" : name,
+            "Email" : email,
+            "Phone" : Phone,
+            "Mandal" : mandal,
+            "31st OCTOBER" : october31,
+            "1st NOVEMBER" : november1,
+            "2nd NOVEMBER" : november2
+        }
+    }
+    response = requests.post(mandal_dict[mandal], json=row)
+    response2 = requests.post(mandal_dict['master_url'], json=row)
+    print(response.status_code)
+    print(response2.status_code)
+
+    return render_template('thankyou.html',value=user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
